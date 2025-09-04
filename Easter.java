be0bf02c2a05d5c65e56f1f708a21659628729eb
carsonKim67 <carson.p.kim@gmail.com>
2025-09-04 15:05:03 +0000

    newenw

--- Easter.java
+++ Easter.java
@@ -1,9 +1,32 @@
+//Carson Kim
+//This program will calculate the date easter falls on given the date:2026
 public class Easter{
-  public static void main(string[] args) {
-
-  }
-    
+  public static void main(String[] args) {
   int y = 2026;
   int a;
-  y%19 = a;
+  int b;
+  int c;
+  int d;
+  int e;
+  int f;
+  int g; 
+  int h;
+  int i;
+  int k;
+  int r;
+  int m;
+  int n;
+  int p;
+a = y%19;
+System.out.println("a = "+a);
+b = y/100;
+c = y%100;
+System.out.println("c = "+c);
+System.out.println("b = "+b);
+d = b/4;
+e = b%4;
+
+  }}
+    
+
 
